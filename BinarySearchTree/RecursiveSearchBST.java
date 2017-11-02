a49753ef3a0b2c73df6ae47c43f7ebd4a50c41e1
Matt Cappleman <matt.cappleman@gmail.com>
2017-11-02 02:13:28 +0000

    Adding the recursive algorithm

---
+++ BinarySearchTree/RecursiveSearchBST.java
@@ -0,0 +1,56 @@
+class RecursiveSearchBST {
+    
+    public static void main(String[] args) {
+
+        int list[] = {0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30};
+
+        if (args.length == 0) {
+            System.out.printf("Please enter a value");
+            return;
+        }
+
+        int lookupValue = Integer.parseInt(args[0]);
+        System.out.printf("%d\n", lookupValue);
+
+        int index =  FindValue(list, lookupValue, 0, list.length-1);
+
+        if (index != -1) {
+
+            System.out.printf("Found %d at index %d", list[index], index);
+
+        } else {
+
+            System.out.printf("%d is not in the current array.", lookupValue);
+
+        }
+
+    }
+
+    private static int FindValue(int[] array, int wantedValue, int start, int end) {
+
+        int mid = (int) Math.floor((start+end)/2);
+
+	System.out.printf("Start: %d, End %d, wantedValue\n", start, end);
+
+	if (array[mid] == wantedValue) {
+            
+	    return mid;
+	
+	} else if (start >= end) {
+
+	    return -1;
+
+	} else if (array[mid] > wantedValue) {
+
+	    end = mid - 1;
+
+	} else if (array[mid] < wantedValue) {
+
+	    start = mid + 1;
+
+	}
+
+	return FindValue(array, wantedValue, start, end);
+
+    }
+}
